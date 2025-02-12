46556313dcf418c19808156174a988027e6a6c38
Suraj <surajg1718@gmail.com>
2025-02-12 10:55:37 +0530

    completed first task

---
+++ task1.py
@@ -0,0 +1,15 @@
+def find_largest(num1, num2, num3):
+    if num1 >= num2 and num1 >= num3:
+        return num1
+    elif num2 >= num1 and num2 >= num3:
+        return num2
+    else:
+        return num3
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+num3 = float(input("Enter the third number: "))
+
+largest = find_largest(num1, num2, num3)
+
+print(f"The largest number is: {largest}")
